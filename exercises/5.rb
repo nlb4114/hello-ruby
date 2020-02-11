0e8e70436fe2b8f65d3807197818c48a0daef849
nlb4114 <nback24@gmail.com>
2020-02-11 03:04:20 +0000

    attempt 2

--- exercises/5.rb
+++ exercises/5.rb
@@ -5,7 +5,7 @@
 # EXERCISE
 # Build a deck of cards. Given the following arrays, use a loop
 # (or two... hint, hint) to write out every combination to the
-# screen.
+# scrern.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
